83262a5d24262c48d88f00609198b79c7302de77
zh-umtyl <zhalgasumtyl@gmail.com>
2026-02-08 00:23:42 +0500

    Force update

--- app/main.py
+++ app/main.py
@@ -1,17 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 import socket
+import psutil
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    # Показываем ID контейнера (hostname), чтобы видеть балансировку в будущем
-    return {
-        "message": "Hello from Docker!", 
-        "hostname": socket.gethostname(),
-        "status": "success"
-    }
+# Папканың жолын нақты көрсетеміз
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "hostname": socket.gethostname()
+    })
+
+@app.get("/api/stats")
+async def get_stats():
+    cpu_usage = psutil.cpu_percent(interval=None)
+    ram_usage = psutil.virtual_memory().percent
+    return {"cpu": cpu_usage, "ram": ram_usage}
+
+# Force update 1
